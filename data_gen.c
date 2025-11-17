6af564295ff527b5d77d54e51651e538a6dd18e3
Kushal Parekh <parekhkushal17@gmail.com>
2025-11-17 16:10:23 -0500

    new checksum variants, gitignore for exe,txt, and counter size increase

--- data_gen.c
+++ data_gen.c
@@ -10,7 +10,7 @@
 
 
 
-uint8_t checksum_4bitCW_8bDW (uint8_t dataword, uint8_t sizeChecksum)
+uint8_t checksum_4bitCW_8bDW_snglPrec (uint8_t dataword, uint8_t sizeChecksum)
 {
     uint8_t returnVal = 0;
 
@@ -23,6 +23,32 @@ uint8_t checksum_4bitCW_8bDW (uint8_t dataword, uint8_t sizeChecksum)
     return (bottomHalf + topHalf) & 0b00001111;
 }
 
+uint8_t checksum_4bitCW_8bDW_doubPrec (uint8_t dataword, uint8_t sizeChecksum)
+{
+    uint8_t bottomHalf = dataword & (0b00001111);
+    uint8_t topHalf = (dataword & (0b11110000)) >> sizeChecksum;   // Make sure both halves align with each other;
+
+    // Return the addition of bottom and top half, but keep 8 bits.
+    return (bottomHalf + topHalf) & 0b11111111;
+}
+
+uint8_t residueChecksum_4bitCW_8bDW_snglPrec (uint8_t dataword, uint8_t sizeChecksum)
+{
+    uint8_t bottomHalf = dataword & (0b00001111);
+    uint8_t topHalf = (dataword & (0b11110000)) >> sizeChecksum;   // Make sure both halves align with each other;
+
+    uint8_t carryIso = (bottomHalf + topHalf) & 0b11110000;
+    uint8_t carry = carryIso >> sizeChecksum;
+
+    return (bottomHalf + topHalf + carry) & 0b00001111;  
+}
+
+uint8_t HWChecksum_4bitCW_8bDW (uint8_t dataword1_2, uint8_t dataword3_4)
+{
+    // innately double precision
+    return dataword1_2 + dataword3_4;
+}
+
 int main()
 {
 
@@ -33,20 +59,21 @@ int main()
 	fptr = fopen("data.txt", "w");
 
 
-    uint8_t counter = 0;
+    uint16_t counter = 0;
     uint8_t keepGoing = 1;
     uint8_t checksum;
     while (keepGoing == 1)
     {
 
-        if (counter == MAX_DW_VALUE)
+        if (counter >= MAX_DW_VALUE)
         {
             keepGoing = 0;
-
+            break;
         }
-        checksum = checksum_4bitCW_8bDW(counter, 4);
+        checksum = checksum_4bitCW_8bDW_snglPrec(counter, counter+1);
 
         fprintf(fptr, "%hhu %hhu\n", counter, checksum);
+        // fprintf(fptr, "%hhu %hhu %hhu\n", counter, counter+1, checksum);
         
         counter++;
     }
