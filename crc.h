dd9270a0b4070538e0cf06e5f461d6bcc7eed3ef
pas218 <pas218@pitt.edu>
2025-12-03 17:39:42 -0500

    Changing computers.

--- crc.h
+++ crc.h
@@ -1,4 +1,27 @@
 #ifndef CRC_H
 #define CRC_H
+#include <stdint.h>
+#define CRC4_BITS 4
+#define CRC4_ITERATIONS 8
+
+// P is hard coded to 11101
+uint8_t crc4_8bit(uint8_t input)
+{
+    int size_P = 5;
+    uint8_t P = 29; // 11101
+    uint8_t crc = 0;
+    uint8_t helper_
+    // Inititial state.
+    for (int i = 0; i < CRC4_BITS; i++)
+    {
+        if ((crc ))
+    }
+    for (int i = 0; i < CRC4_ITERATIONS; i++)
+    {
+
+    }
+    
+    return crc & 0xF; 
+}
 
 #endif
